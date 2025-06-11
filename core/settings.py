675ecb78cfb9c13d1d8883fca1a7a9be01bc2ea3
anihpatrickugo <iampatrickugo@gmail.com>
2025-06-11 16:45:58 +0100

    fixed

--- core/settings.py
+++ core/settings.py
@@ -191,7 +191,7 @@ CHANNEL_LAYERS = {
 
 
 # Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+# https://docs.djangoproject.com/en/5.0/ref/settings/#database
 #
 # DATABASES = {
 #     "default": {
