78c7fa80ba7701e50c92308602678cac26df2684
anihpatrickugo <iampatrickugo@gmail.com>
2025-06-13 14:43:18 +0100

    added whitenoise middleware

--- core/settings.py
+++ core/settings.py
@@ -132,6 +132,7 @@ SPECTACULAR_SETTINGS = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
