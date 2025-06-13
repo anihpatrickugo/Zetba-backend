dafd1eac0a2130e4ed15e3a4c72dc3d1ce6baa09
anihpatrickugo <iampatrickugo@gmail.com>
2025-06-13 12:32:13 +0100

    fixed asgi app import order again

--- core/asgi.py
+++ core/asgi.py
@@ -8,16 +8,19 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+from django.core.asgi import get_asgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
+
 django_asgi_app = get_asgi_application()
 
 from .middleware import JWTAuthMiddlewareStack
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
+
 
 
 from users.routing import websocket_urlpatterns
