09af3dbae3533e7b6a144865a0487241b63c20d4
anihpatrickugo <iampatrickugo@gmail.com>
2025-06-13 15:05:21 +0100

    corrected storges dictionary

--- core/settings.py
+++ core/settings.py
@@ -264,11 +264,9 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STORAGES = {
-    "staticfiles": {
+    "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
